c3534b98dc9732f226ba369ecec7f17558ec16a0
Steven McGrath <mcgrath.steven7@gmail.com>
2025-03-15 16:10:05 -0400

    Added Head and removed theme toggle for now

--- components/typography/CopyText.tsx
+++ components/typography/CopyText.tsx
@@ -4,8 +4,6 @@ import styled from "styled-components";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { Document } from "@contentful/rich-text-types";
 
-import mq from "../../styles/theme/mq";
-
 interface CopyProps {
     content: {
       json: Document;
@@ -13,13 +11,12 @@ interface CopyProps {
 }
 
 const StyledParagraph = styled.p`
+  font-size: 1rem;
   margin-bottom: var(--space-4);
   padding-inline: var(--space-12);
   text-align: justify;
-  @media ${mq.mobile} {
-    padding-inline: var(--space-2);
-  }
 `;
+
 
 export default function CopyText({ content }: CopyProps) {
   return (
